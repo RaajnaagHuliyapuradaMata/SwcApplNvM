eb047c144bf852c232e9d310a7924e9ad6bcd2ed
NagarajaHuliyapuradaMata <nagaraja.hm.0001@gmail.com>
2024-02-15 11:44:21 +0100

    Daily back-up

--- EcuResim_vVcBsBpTpms_cB_LATEST/b_git_push.sh
+++ EcuResim_vVcBsBpTpms_cB_LATEST/b_git_push.sh
@@ -1,4 +1,4 @@
 git add .
 git status
-git commit -m "AUTOSAR Migration Activities - SwcApplNvM - EcuResim_vVcBsBpTpms_cB_LATEST"
+git commit -m "AUTOSAR Migration Activities - SwcApplNvM - vEcu_vVcBsBpTpms_cB_LATEST"
 git push
